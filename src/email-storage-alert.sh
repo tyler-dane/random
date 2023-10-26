7da24140d1a37141371c79837bfd710b840e9845
Tyler Dane <tyler@switchback.tech>
2023-10-26 07:47:33 -0500

    docs: email-storage

--- src/email-storage-alert.sh
+++ src/email-storage-alert.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# Author: Tyler Hitzeman
-# Sends email to tyler.hitzeman@gmail.com whenever disk space is over 80%
+# Sends email to tyler@gmail.com whenever disk space is over 80%
 # Cron configured to execute this daily
 # Requires the 'mailx' package.
 #   On RedHat systems, run `yum install mailx` to install
@@ -13,6 +12,6 @@ do
   partition=$(echo $output | awk '{ print $2 }' )
   if [ $usep -ge 80 ]; then
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
-     mail -s "Alert: $usep% of disk space used." tyler.hitzeman@gmail.com
+     mail -s "Alert: $usep% of disk space used." tyler@gmail.com
   fi
 done
